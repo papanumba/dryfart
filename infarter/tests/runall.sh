0ae4c7aed1e15ba57ffb825ecf558016e9557997
papanumba <orive2002@gmail.com>
2023-09-26 15:31:31 +0200

    fixed copypasta error in runall.sh

--- infarter/tests/runall.sh
+++ infarter/tests/runall.sh
@@ -4,9 +4,9 @@ for f in $(ls *.df)
 do
     ../target/debug/infarter $f > /dev/null 2> /dev/null
     if [ $? -eq 0 ];
-     then
+    then
         echo "PASS $f"
     else
-        echo "PASS $f"
+        echo "FAIL $f"
     fi
 done
