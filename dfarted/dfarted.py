4e29aecb532b79a1b1dd800b76bdfca64fa3790f
papanumba <orive2002@gmail.com>
2023-10-14 20:28:34 +0200

    dfarted 1st commit

---
+++ dfarted/dfarted.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+
+################################################################################
+###############################  D F a r t E d  ################################
+################################################################################
+
+
+import sys, os, subprocess
+from PyQt5 import uic, QtGui
+from PyQt5.QtWidgets import *
+import hieliter
+
+
+def get_name(f):
+    if f is None:
+        return "New File"
+    else:
+        return os.path.basename(f)
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self) # load the qt5 gui
+        self.editor.setStyleSheet("""QPlainTextEdit{
+            font-family:'Monospace';
+            font-size: 16pt;
+            color: #e2e2e2;
+            background-color: #1B1B1B;}"""
+        )
+        self.output.setStyleSheet("""QPlainTextEdit{
+            font-family:'Monospace';
+            font-size: 14pt;
+            color: #e2e2e2;
+            background-color: #1b1b1b;}"""
+        )
+        self.edit_file = None
+        self.temp_file = None
+        self.saved = False
+        self.update_title() # "New File"
+        # set the highlighting to the editor widget
+        self.highlight = hieliter.DFHieliter(self.editor.document())
+        self.button_open.clicked.connect(self.open_file)
+        self.button_save.clicked.connect(self.save_file)
+        self.button_new.clicked.connect(self.new_file)
+        # set analize() to the button_analize
+        self.button_analize.clicked.connect(self.analize)
+        self.sc_save = QShortcut(QtGui.QKeySequence("Ctrl+S"), self)
+        self.sc_save.activated.connect(self.save_file)
+
+    def update_title(self):
+        self.setWindowTitle("DFartEd - " + get_name(self.edit_file))
+
+    def new_file(self):
+        # edited opened file must be saved
+        if self.edit_file is not None:
+            self.save_file()
+        # edited new file w/ chars other than whitespace must be saved
+        else:
+            if self.editor.toPlainText().strip() != '':
+                self.save_file()
+        self.saved = False
+        self.edit_file = None
+        self.temp_file = None
+        self.editor.setPlainText('')
+        self.output.setPlainText('')
+        self.update_title()
+
+    def open_file(self):
+        if self.temp_file is not None:
+#            subprocess.run(["rm", self.temp_file])
+            self.temp_file = None
+        name = QFileDialog.getOpenFileName(self, 'Open File', '',
+            'DryFart script (*.df)')
+        if name == '': # pressed `Cancel`
+            return
+        self.edit_file = name[0]
+        self.saved = False
+        openfile = open(self.edit_file, 'r')
+        self.update_title()
+        self.editor.setPlainText(openfile.read())
+        openfile.close()
+        self.temp_file = name[0] + ".tmp"
+
+    def save_file(self):
+        if self.saved:
+            return
+        if self.edit_file is None or self.edit_file == '':
+            name = QFileDialog.getSaveFileName(
+                self, 'Save File', '', 'DryFart script (*.df)'
+            )
+            if name == '': # pressed `Cancel`
+                return
+            self.edit_file = name[0]
+            self.setWindowTitle("DFartEd - " + get_name(self.edit_file))
+        savefile = open(self.edit_file,'w')
+        savefile.write(self.editor.toPlainText())
+        savefile.close()
+        self.saved = True
+
+    def analize(self):
+        if self.edit_file is None:
+            self.temp_file = ".tmp.df"
+        # save a temp file, then send it to infarter & output result
+        tempfile = open(self.temp_file, 'w')
+        tempfile.write(self.editor.toPlainText())
+        tempfile.close()
+        result = subprocess.run(
+            ["../infarter/target/debug/infarter", self.temp_file],
+            capture_output=True
+        )
+        if result.stderr == b'':
+            self.output.setPlainText(result.stdout.decode("utf-8"))
+        else:
+            self.output.setPlainText(
+                "ERROR from InFarter\n"+result.stderr.decode("utf-8")
+            )
+
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    gui = Main()
+    gui.show()
+    sys.exit(app.exec_())
---
+++ dfarted/hieliter.py
@@ -0,0 +1,102 @@
+# hieliter.py
+
+#import sys
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter
+
+
+# returns a QTextCharFormat with the given attributes
+def format(color, style=''):
+    _color = QColor()
+    if type(color) is not str:
+        _color.setRgb(color[0], color[1], color[2])
+    else:
+        _color.setNamedColor(color)
+    format = QTextCharFormat()
+    format.setForeground(_color)
+    if 'bold' in style:
+        format.setFontWeight(QFont.Bold)
+    if 'italic' in style:
+        format.setFontItalic(True)
+    return format
+
+
+# luma: best 70
+# chroma: best 60
+colors = {
+    'black':    [  0,   0,   0],
+    'red':      [254, 119, 112],
+    'green':    [ 56, 194, 113],
+    'yellow':   [194, 169,  52],
+    'blue':     [124, 174, 255],
+    'magenta':  [241, 132, 227],
+    'cyan':     [ 22, 189, 207],
+    'white':    [171, 171, 171],
+    'bblack':   [119, 119, 119],
+}
+
+'''    'bred'
+    'bgreen'
+    'byellow'
+    'bblue'
+    'bmagenta'
+    'bcyan'
+    'bwhite' '''
+
+# Syntax styles specific by token type
+STYLES = {
+    'func':     format(colors['green']  , 'bold'),
+    'proc':     format(colors['blue']   , 'bold'),
+    'control':  format(colors['red']    , 'bold'),
+    'number':   format(colors['cyan'])  ,
+    'string':   format(colors['yellow']),
+    'type':     format(colors['magenta'], 'bold'),
+    'comment':  format(colors['bblack'] , 'italic'),
+}
+
+
+class DFHieliter(QSyntaxHighlighter):
+    def __init__(self, document):
+        QSyntaxHighlighter.__init__(self, document)
+        rules = [
+            # types
+            (r'\b[BCNZR]%', 0, STYLES['type']),
+            # Numeric literals
+            (r'(\b|_)\d+(\.\d+)?(E[+-]?\d+)?(\b|_)', 0, STYLES['number']),
+            # this last don't detect ".5" style numbers,
+            # and in dryfart, the R% values are written: \d+\.\d+
+            # (if) then
+            (r'=>', 0, STYLES['control']),
+            # loop
+            (r'\.?@', 0, STYLES['control']),
+            # function call 'word#'
+            (r'(\b|_)[A-Za-z][0-9A-Za-z]*#', 0, STYLES['func']),
+            # function types contain '#'
+            (r'#', 0, STYLES['func']),
+            # procedure decl !word!
+            (r'[A-Za-z][0-9A-Za-z]*!',0,STYLES['proc']),
+            (r'!',0,STYLES['proc']),
+            #uscore
+            #(r'_', 0, STYLES['text']),
+            # Double-quoted string, possibly containing escape sequences (X$)
+            (r'"([^"$]*["$NT]\$)*[^"$]*"', 0, STYLES['string']),
+            # word between <> (inside string)
+#            (r'<[A-Za-z][0-9A-Za-z]*>', 0, STYLES['number'])
+            # Simple comment: from '' until a newline
+            (r"'[^\n]*", 0, STYLES['comment'])
+        ]
+        # Build a QRegExp for each pattern
+        self.rules = [(QRegExp(pat), index, fmt)
+                      for (pat, index, fmt) in rules]
+
+    # Apply syntax highlighting to the given block of text.
+    def highlightBlock(self, text):
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+            while index >= 0:
+                # We actually want the index of the nth match
+                index = expression.pos(nth)
+                length = len(expression.cap(nth))
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+        self.setCurrentBlockState(0)
