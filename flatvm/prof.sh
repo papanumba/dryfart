7eb2be844d9729fbd014283cd824a6fda4bd834e
papanumba <orive2002@gmail.com>
2024-03-01 17:40:42 +0100

    before branch

--- flatvm/prof.sh
+++ flatvm/prof.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gcc -pg -Iinclude -O3 -ansi src/*.c -o gprof-a.out
+gcc -pg -g -Iinclude -O3 -std=c99 src/*.c -o gprof-a.out
 ./gprof-a.out "$@"
 gprof gprof-a.out gmon.out > gprof-out.txt
