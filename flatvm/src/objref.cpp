54385824fed911908efcf2f64136a681984b1f18
papanumba <orive2002@gmail.com>
2024-06-28 17:26:45 +0200

    some cleaning

--- flatvm/src/objref.cpp
+++ flatvm/src/objref.cpp
@@ -14,15 +14,17 @@ bool ObjRef::operator!=(const ObjRef &that) const
     return !(*this == that);
 }
 
+#define BASURA_CASES \
+      BASURA(ARR, arr) \
+      BASURA(TBL, tbl) \
+      BASURA(FUN, fun) \
+      BASURA(PRO, pro)
+
 bool ObjRef::get_gc_mark() const
 {
     switch (this->typ()) {
-#define BASURA(TTT, ttt) \
-      case OBJ_##TTT: return this->as_##ttt()->gc_mark;
-      BASURA(ARR, arr)
-      BASURA(TBL, tbl)
-      BASURA(FUN, fun)
-      BASURA(PRO, pro)
+#define BASURA(TTT, ttt) case OBJ_##TTT: return this->as_##ttt()->gc_mark;
+      BASURA_CASES
 #undef BASURA
     }
 }
@@ -30,12 +32,8 @@ bool ObjRef::get_gc_mark() const
 void ObjRef::set_gc_mark(bool m = true)
 {
     switch (this->typ()) {
-#define BASURA(TTT, ttt) \
-      case OBJ_##TTT: this->as_##ttt()->gc_mark = m; break;
-      BASURA(ARR, arr)
-      BASURA(TBL, tbl)
-      BASURA(FUN, fun)
-      BASURA(PRO, pro)
+#define BASURA(TTT, ttt) case OBJ_##TTT: this->as_##ttt()->gc_mark = m; break;
+      BASURA_CASES
 #undef BASURA
     }
 }
@@ -43,9 +41,10 @@ void ObjRef::set_gc_mark(bool m = true)
 void ObjRef::print() const
 {
     switch (this->typ()) {
-      case OBJ_ARR: this->as_arr()->print(); break;
-      case OBJ_TBL: this->as_tbl()->print(); break;
-      case OBJ_FUN: this->as_fun()->print(); break;
-      case OBJ_PRO: this->as_pro()->print(); break;
+#define BASURA(TTT, ttt) case OBJ_##TTT: this->as_##ttt()->print(); break;
+      BASURA_CASES
+#undef BASURA
     }
 }
+
+#undef BASURA_CASES
